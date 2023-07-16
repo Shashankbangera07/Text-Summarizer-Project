26dc5c87c8a4b60ff4e85c2e5f7727ca5832f5d7
shashankbangera07 <shashankspam52@gmail.com>
2023-07-16 20:50:48 +0530

    updates

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "Shashank"
+AUTHOR_USER_NAME = "Shashankbangera07"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "shashankspam52@gmail.com"
 
